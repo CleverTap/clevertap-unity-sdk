d95c918064f69690659131c548e233cddde4e49d
Aditi Agrawal <Aditi3@users.noreply.github.com>
2021-05-03 11:58:52 +0530

    Update CleverTapBinding.cs

--- Plugin/CleverTapUnity/CleverTapUnity-Scripts/CleverTapBinding.cs
+++ Plugin/CleverTapUnity/CleverTapUnity-Scripts/CleverTapBinding.cs
@@ -11,7 +11,7 @@ using CleverTap.Utilities;
 namespace CleverTap {
   public class CleverTapBinding : MonoBehaviour {
       
-    public const string Version = "1.2.5";
+    public const string Version = "2.1.2";
 
 #if UNITY_IOS
     void Start() {
