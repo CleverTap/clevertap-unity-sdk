7fcc997fdd128418883f8bc734fe73a19ed079fb
william <william@clevertap.com>
2023-04-11 00:55:55 +0530

    chore(SDK-2868)-refactor `CleverTapInboxItemClicked` callback with additional params

--- src/Android/CleverTapUnityPlugin/clevertapsdk-unity/src/main/java/com/clevertap/unity/CleverTapUnityPlugin.java
+++ src/Android/CleverTapUnityPlugin/clevertapsdk-unity/src/main/java/com/clevertap/unity/CleverTapUnityPlugin.java
@@ -822,7 +822,7 @@ public class CleverTapUnityPlugin implements SyncListener, InAppNotificationList
         if (message != null && message.getData() != null) {
             JSONObject jsonObject = new JSONObject();
             try {
-                jsonObject.put("CTInboxMessagePayload",message.getData().toString());
+                jsonObject.put("CTInboxMessagePayload",message.getData());
                 jsonObject.put("ContentPageIndex",contentPageIndex);
                 jsonObject.put("ButtonIndex",buttonIndex);
                 messageUnity(CLEVERTAP_GAME_OBJECT_NAME, CLEVERTAP_ON_INBOX_ITEM_CLICKED,
