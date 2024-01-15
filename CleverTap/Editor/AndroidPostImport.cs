4b1322a85d6a00dfd507449ec24ad680dae77a2e
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-01-15 16:45:00 +0200

    iOS Post Build processor and settings window (#64)
    * Add Post Build processor for iOS. Add settings window for the config.
    
    * Add to namespace and format

--- CleverTap/Editor/AndroidPostImport.cs
+++ CleverTap/Editor/AndroidPostImport.cs
@@ -1,10 +1,16 @@
 using System.Linq;
 using UnityEditor;
 
-class AndroidPostImport : AssetPostprocessor {
-    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload) {
-        if (AssetDatabase.IsValidFolder("Assets/CleverTap/Plugins/Android/clevertap-android-wrapper") && importedAssets?.Length > 0 && importedAssets.Contains("Assets/CleverTap/Plugins/Android/clevertap-android-wrapper")) {
-            AssetDatabase.MoveAsset("Assets/CleverTap/Plugins/Android/clevertap-android-wrapper", "Assets/CleverTap/Plugins/Android/clevertap-android-wrapper.androidlib");
+namespace CleverTapSDK.Private
+{
+    class AndroidPostImport : AssetPostprocessor
+    {
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
+        {
+            if (AssetDatabase.IsValidFolder("Assets/CleverTap/Plugins/Android/clevertap-android-wrapper") && importedAssets?.Length > 0 && importedAssets.Contains("Assets/CleverTap/Plugins/Android/clevertap-android-wrapper"))
+            {
+                AssetDatabase.MoveAsset("Assets/CleverTap/Plugins/Android/clevertap-android-wrapper", "Assets/CleverTap/Plugins/Android/clevertap-android-wrapper.androidlib");
+            }
         }
     }
 }
