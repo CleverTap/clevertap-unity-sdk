e1a994a9a791dc16d5170a5fea7d6510be32442f
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-01-15 21:55:30 +0200

    Prepare 3.0.0 release (#65)
    * Prepare 3.0.0 release
    
    * Update README.md
    
    * Update android-wrapper build.gradle
    
    * Update package

--- CleverTap/Runtime/Constants/CleverTapVersion.cs
+++ CleverTap/Runtime/Constants/CleverTapVersion.cs
@@ -1,9 +1,5 @@
 namespace CleverTapSDK.Constants {
     internal static class CleverTapVersion {
-<<<<<<< HEAD
         internal const string CLEVERTAP_SDK_VERSION = "3.0.0";
-=======
-        internal const string CLEVERTAP_SDK_VERSION = "2.4.3";
->>>>>>> 58f5a2b (Feature/sdk 3323/variables bindings (#54))
     }
 }
