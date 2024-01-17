617ab7632df0c1498b7258150404aaf3f0613d1b
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-01-17 21:14:55 +0200

    Update settings to use an asset

--- CleverTap/Editor/CleverTapSettingsWindow.cs
+++ CleverTap/Editor/CleverTapSettingsWindow.cs
@@ -3,48 +3,72 @@ using UnityEditor;
 
 namespace CleverTapSDK.Private
 {
-    // Custom editor window class
     public class CleverTapSettingsWindow : EditorWindow
     {
-        private const string NAME = "CleverTap Settings";
+        internal const string ITEM_NAME = "Assets/CleverTap Settings";
+        private static readonly string windowName = "CleverTap Settings";
+        private static readonly string settingsPath = "Assets/CleverTapSettings.asset";
 
-        private IOSConfigSettings config;
+        private CleverTapSettings settings;
 
         // Add menu item to open the window
-        [MenuItem("Assets/" + NAME)]
+        [MenuItem(ITEM_NAME)]
         public static void ShowWindow()
         {
-            GetWindow<CleverTapSettingsWindow>(NAME);
+            GetWindow<CleverTapSettingsWindow>(windowName);
         }
 
         private void OnEnable()
         {
-            // Load settings from EditorPrefs
-            config = IOSConfigSettings.Load();
+            settings = LoadCleverTapSettings();
         }
 
         private void OnGUI()
         {
-            // iOS Settings
-            GUILayout.Label("iOS Settings", EditorStyles.boldLabel);
+            GUILayout.Label(windowName, EditorStyles.boldLabel);
 
             // Input text fields
-            config.CleverTapAccountId = EditorGUILayout.TextField("CleverTapAccountId", config.CleverTapAccountId);
-            config.CleverTapAccountToken = EditorGUILayout.TextField("CleverTapAccountToken", config.CleverTapAccountToken);
-            config.CleverTapAccountRegion = EditorGUILayout.TextField("CleverTapAccountRegion", config.CleverTapAccountRegion);
+            settings.CleverTapAccountId = EditorGUILayout.TextField("CleverTapAccountId", settings.CleverTapAccountId);
+            settings.CleverTapAccountToken = EditorGUILayout.TextField("CleverTapAccountToken", settings.CleverTapAccountToken);
+            settings.CleverTapAccountRegion = EditorGUILayout.TextField("CleverTapAccountRegion", settings.CleverTapAccountRegion);
 
             // Checkboxes
-            config.CleverTapEnablePersonalization = EditorGUILayout.Toggle("CleverTapEnablePersonalization", config.CleverTapEnablePersonalization);
-            config.CleverTapDisableIDFV = EditorGUILayout.Toggle("CleverTapDisableIDFV", config.CleverTapDisableIDFV);
+            settings.CleverTapEnablePersonalization = EditorGUILayout.Toggle("CleverTapEnablePersonalization", settings.CleverTapEnablePersonalization);
+            settings.CleverTapDisableIDFV = EditorGUILayout.Toggle("CleverTapDisableIDFV", settings.CleverTapDisableIDFV);
 
             // Save button
             if (GUILayout.Button("Save Settings"))
             {
                 // Save settings to EditorPrefs
-                config.Save();
+                SaveCleverTapSettings();
 
-                Debug.Log($"{NAME} saved!");
+                Debug.Log($"{windowName} saved!");
             }
+        }
+
+        private static CleverTapSettings LoadCleverTapSettings()
+        {
+            // Load settings from .asset file
+            CleverTapSettings settings = AssetDatabase.LoadAssetAtPath<CleverTapSettings>(CleverTapSettings.settingsPath);
+
+            if (settings == null)
+            {
+                Debug.Log("Asset not found. Creating asset.");
+                // Create a new instance if it doesn't exist
+                settings = CreateInstance<CleverTapSettings>();
+                AssetDatabase.CreateAsset(settings, settingsPath);
+                AssetDatabase.SaveAssets();
+                // Refresh the database to make sure the new asset is recognized
+                AssetDatabase.Refresh();
+            }
+            return settings;
+        }
+
+        private void SaveCleverTapSettings()
+        {
+            // Save settings to .asset file
+            EditorUtility.SetDirty(settings);
+            AssetDatabase.SaveAssetIfDirty(settings);
         }
     }
 }
