82a7f616338df547177534d556b6714a9bab7e4b
Alok Kumar <alok@clevertap.com>
2024-10-10 16:45:09 +0530

    package and folder changes

--- CleverTap/Runtime/Native/UnityNativeWrapper/UnityNativeEventManager.cs
+++ CleverTap/Runtime/Native/UnityNativeWrapper/UnityNativeEventManager.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Native.UnityNativeWrapper.Models;
 using UnityEngine;
 
 namespace CleverTapSDK.Native {
