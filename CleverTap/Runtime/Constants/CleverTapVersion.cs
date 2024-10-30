b5410c9e65a906a297a129568a78db597c808549
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-10-30 19:12:57 +0200

    Make SDK version public

--- CleverTap/Runtime/Constants/CleverTapVersion.cs
+++ CleverTap/Runtime/Constants/CleverTapVersion.cs
@@ -1,6 +1,6 @@
 namespace CleverTapSDK.Constants {
-    internal static class CleverTapVersion {
-        internal const string CLEVERTAP_SDK_VERSION = "4.0.0";
-        internal const string CLEVERTAP_SDK_REVISION = "40000";
+    public static class CleverTapVersion {
+        public const string CLEVERTAP_SDK_VERSION = "4.0.0";
+        public const string CLEVERTAP_SDK_REVISION = "40000";
     }
 }
