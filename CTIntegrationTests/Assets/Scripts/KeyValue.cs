3e03d6599f2bb5542f68d844fdbec6d4c195d69d
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-10-30 13:59:54 +0200

    Add KeyValue prefab

---
+++ CTIntegrationTests/Assets/Scripts/KeyValue.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+
+namespace CTIntegrationTests
+{
+    public class KeyValue : MonoBehaviour
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public bool UseTextValues;
+        public TMP_Text KeyText;
+        public TMP_Text ValueText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (!UseTextValues)
+            {
+                KeyText.SetText(Key);
+                ValueText.SetText(Value);
+            }
+
+            RefreshContentHelper.RefreshContentFitters((RectTransform)transform.parent.transform);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
