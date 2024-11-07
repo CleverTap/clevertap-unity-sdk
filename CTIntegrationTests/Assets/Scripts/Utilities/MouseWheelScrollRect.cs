aadab405e352caa5a20e485074da142a3354f5d8
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-11-07 12:56:36 +0200

    Add summary for custom scroll rect

--- CTIntegrationTests/Assets/Scripts/Utilities/MouseWheelScrollRect.cs
+++ CTIntegrationTests/Assets/Scripts/Utilities/MouseWheelScrollRect.cs
@@ -2,11 +2,20 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-// MouseWheelScrollRect
-// https://discussions.unity.com/t/scroll-view-does-not-scroll-with-mousewheel-when-mouse-is-over-a-button-inside-the-scroll-view/781199/3
+/// <summary>
+/// ScrollView that enables mousewheel and touchpad scrolling.
+/// Enables scrolling over elements inside the scroll view.
+///
+/// Use this component instead of the default ScrollRect component on a ScrollView.
+/// 
+/// Script is based on https://discussions.unity.com/t/scroll-view-does-not-scroll-with-mousewheel-when-mouse-is-over-a-button-inside-the-scroll-view/781199/3
+/// 
+/// Note that to scroll over the ContentView, the ContentView needs to have a raycast target.
+/// Ensure it has a graphic or image component and raycast target is enabled. It should also have a Canvas Renderer in that case.
+/// </summary>
 public class MouseWheelScrollRect : ScrollRect, IPointerEnterHandler, IPointerExitHandler
 {
-    private static string mouseScrollWheelAxis = "Mouse ScrollWheel";
+    private static readonly string mouseScrollWheelAxis = "Mouse ScrollWheel";
     private bool swallowMouseWheelScrolls = true;
     private bool isMouseOver = false;
 
