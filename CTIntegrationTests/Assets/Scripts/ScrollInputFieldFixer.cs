f4f1368517f4be81e189c64a4c8a83dd181d585a
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-11-07 12:55:56 +0200

    Fix selection inside input

--- CTIntegrationTests/Assets/Scripts/ScrollInputFieldFixer.cs
+++ CTIntegrationTests/Assets/Scripts/ScrollInputFieldFixer.cs
@@ -5,55 +5,67 @@ using UnityEngine.UI;
 
 namespace CTIntegrationTests
 {
+    /// <summary>
+    /// Enable scroll over InputFields inside ScrollView.
+    /// 
+    /// The default behaviour pops up the keyboard on mobile and selects/focuses the input field on web/desktop.
+    /// The mouse wheel scroll stops when it reaches an input field.
+    /// This script fixes the above behavior.
+    /// </summary>
     [RequireComponent(typeof(TMP_InputField))]
-    public class ScrollInputFieldFixer : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerClickHandler
+    public class ScrollInputFieldFixer : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerUpHandler
     {
         private ScrollRect _scrollRect = null;
         private TMP_InputField _input = null;
         private bool _isDragging = false;
+        private bool _preventScrollRectDrag;
 
         private void Start()
         {
             _scrollRect = GetComponentInParent<ScrollRect>();
+            if (_scrollRect == null)
+            {
+                Debug.LogWarning("[ScrollInputFieldFixer]: ScrollRect not found.");
+            }
+
             _input = GetComponent<TMP_InputField>();
             _input.DeactivateInputField();
+            _input.onDeselect.AddListener(_ => _preventScrollRectDrag = false);
         }
 
         public void OnBeginDrag(PointerEventData data)
         {
-            if (_scrollRect != null && _input != null)
+            if (_scrollRect != null && !_preventScrollRectDrag)
             {
                 _isDragging = true;
                 _input.DeactivateInputField();
-                _scrollRect.SendMessage("OnBeginDrag", data);
+                _scrollRect.OnBeginDrag(data);
             }
         }
 
         public void OnEndDrag(PointerEventData data)
         {
-            if (_scrollRect != null && _input != null)
+            if (_scrollRect != null)
             {
                 _isDragging = false;
-                _scrollRect.SendMessage("OnEndDrag", data);
+                _scrollRect.OnEndDrag(data);
             }
         }
 
         public void OnDrag(PointerEventData data)
         {
-            if (_scrollRect != null && _input != null)
+            if (_scrollRect != null)
             {
-                _scrollRect.SendMessage("OnDrag", data);
+                _scrollRect.OnDrag(data);
             }
         }
 
-        public void OnPointerClick(PointerEventData data)
+        public void OnPointerUp(PointerEventData data)
         {
-            if (_scrollRect != null && _input != null)
+            if (!_isDragging && !data.dragging)
             {
-                if (!_isDragging && !data.dragging)
-                {
-                    _input.ActivateInputField();
-                }
+                _input.ActivateInputField();
+                _preventScrollRectDrag = true;
             }
         }
     }
