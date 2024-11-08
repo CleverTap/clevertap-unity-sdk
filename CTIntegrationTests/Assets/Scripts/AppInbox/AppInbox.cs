868ac012de646cbfe9250a5a490cdc4b46953f01
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-11-08 17:06:06 +0200

    Move ButtonActionModel to separate file

--- CTIntegrationTests/Assets/Scripts/AppInbox/AppInbox.cs
+++ CTIntegrationTests/Assets/Scripts/AppInbox/AppInbox.cs
@@ -6,26 +6,6 @@ using UnityEngine.UI;
 
 namespace CTIntegrationTests
 {
-    internal delegate void ButtonAction(Button button);
-
-    internal class ButtonActionModel
-    {
-        internal string Name { get; set; }
-        internal string Tag { get; set; }
-        internal ButtonAction Action { get; set; }
-
-        internal ButtonActionModel(string name, ButtonAction action) : this(name, null, action)
-        {
-        }
-
-        internal ButtonActionModel(string name, string tag, ButtonAction action)
-        {
-            Name = name;
-            Tag = tag;
-            Action = action;
-        }
-    }
-
     public class AppInbox : MonoBehaviour
     {
         public Button buttonPrefab;
---
+++ CTIntegrationTests/Assets/Scripts/Utilities/ButtonActionModel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+
+namespace CTIntegrationTests
+{
+    internal delegate void ButtonAction(Button button);
+
+    internal class ButtonActionModel
+    {
+        internal string Name { get; set; }
+        internal string Tag { get; set; }
+        internal ButtonAction Action { get; set; }
+
+        internal ButtonActionModel(string name, ButtonAction action) : this(name, null, action)
+        {
+        }
+
+        internal ButtonActionModel(string name, string tag, ButtonAction action)
+        {
+            Name = name;
+            Tag = tag;
+            Action = action;
+        }
+    }
+}
