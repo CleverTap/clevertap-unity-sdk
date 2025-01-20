819eac77665c8ff5b9160cc62cf4edaa0058f66b
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-01-20 23:38:10 +0200

    Add additional in-app message buttons

--- CTExample/Assets/Scripts/QA SDK/MessagesMenu.cs
+++ CTExample/Assets/Scripts/QA SDK/MessagesMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CleverTapSDK;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,50 @@ namespace CTExample
         // Use this for initialization
         void Start()
         {
+            var parent = verticalLayoutGroup.GetComponent<RectTransform>();
+
+            string suspend = "Suspend In-apps";
+            var suspendButton = Instantiate(buttonPrefab);
+            suspendButton.name = suspend;
+            suspendButton.transform.SetParent(parent, false);
+            suspendButton.GetComponentInChildren<Text>().text = suspend;
+            suspendButton.onClick.AddListener(() =>
+            {
+                CleverTap.SuspendInAppNotifications();
+            });
+
+            string discard = "Discard In-apps";
+            var discardButton = Instantiate(buttonPrefab);
+            discardButton.name = discard;
+            discardButton.transform.SetParent(parent, false);
+            discardButton.GetComponentInChildren<Text>().text = discard;
+            discardButton.onClick.AddListener(() =>
+            {
+                CleverTap.DiscardInAppNotifications();
+            });
+
+            string resume = "Resume In-apps";
+            var resumeButton = Instantiate(buttonPrefab);
+            resumeButton.name = resume;
+            resumeButton.transform.SetParent(parent, false);
+            resumeButton.GetComponentInChildren<Text>().text = resume;
+            resumeButton.onClick.AddListener(() =>
+            {
+                CleverTap.ResumeInAppNotifications();
+            });
+
+            string sync = "Sync Custom Templates";
+            var syncButton = Instantiate(buttonPrefab);
+            syncButton.name = sync;
+            syncButton.transform.SetParent(parent, false);
+            syncButton.GetComponentInChildren<Text>().text = sync;
+            syncButton.onClick.AddListener(() =>
+            {
+                CleverTap.SyncCustomTemplates(true);
+            });
+
+            AddSeparator(parent);
+
             var messages = new List<string>(new string[] {
                 "Cover",
                 "Interstitial",
@@ -25,8 +70,6 @@ namespace CTExample
                 "Custom HTML"
             });
 
-            var parent = verticalLayoutGroup.GetComponent<RectTransform>();
-
             foreach (var message in messages)
             {
                 var button = Instantiate(buttonPrefab);
@@ -35,12 +78,25 @@ namespace CTExample
                 button.GetComponentInChildren<Text>().text = message;
                 button.onClick.AddListener(() =>
                 {
-                    CleverTapSDK.CleverTap.RecordEvent(button.name);
+                    CleverTap.RecordEvent(button.name);
                     Toast.Show($"Recording event: {button.name}");
                 });
             }
 
             RefreshContentHelper.RefreshContentFitters(parent);
+        }
+
+        void AddSeparator(Transform parent)
+        {
+            var separator = new GameObject();
+            separator.name = "separator";
+            RectTransform rt = separator.AddComponent<RectTransform>();
+
+            // Use a constant value or use the button y
+            // buttonPrefab.GetComponent<RectTransform>().sizeDelta.y
+            float y = 80;
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, y);
+            separator.transform.SetParent(parent, false);
         }
     }
 }
