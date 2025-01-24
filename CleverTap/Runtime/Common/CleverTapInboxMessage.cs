7f54e3e63ee3709b9eb40a1197fba3ce0198dcc1
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-01-24 18:35:57 +0200

    Add summary comments

--- CleverTap/Runtime/Common/CleverTapInboxMessage.cs
+++ CleverTap/Runtime/Common/CleverTapInboxMessage.cs
@@ -11,6 +11,9 @@ namespace CleverTapSDK
         /// </summary>
         public string Id { get; set; }
 
+        /// <summary>
+        /// The inbox "msg" Message data.
+        /// </summary>
         public MessageData Message { get; set; }
 
         /// <summary>
@@ -29,12 +32,13 @@ namespace CleverTapSDK
         public DateTime? DateUtcDate => DateTs > 0 ? DateTimeOffset.FromUnixTimeSeconds(DateTs).UtcDateTime : null;
 
         /// <summary>
-        /// Returns the expiry timestamp of the inbox message.
+        /// Returns the expiry timestamp (time to live) of the inbox message or
+        /// 0 if time to live is set to infinite.
         /// </summary>
         public long ExpiresTs { get; set; }
 
         /// <summary>
-        /// Returns the expiry UTC date of the inbox message.
+        /// Returns the expiry UTC date of the inbox message or null if no expiry.
         /// </summary>
         public DateTime? ExpiresUtcDate => ExpiresTs > 0 ? DateTimeOffset.FromUnixTimeSeconds(ExpiresTs).UtcDateTime : null;
 
@@ -48,10 +52,34 @@ namespace CleverTapSDK
         #region Inbox Message nested classes
         public class MessageData
         {
+            /// <summary>
+            /// The inbox message type. Possible values are "simple",
+            /// "message-icon", "carousel", "carousel-image".
+            /// </summary>
             public string MessageType { get; set; }
+
+            /// <summary>
+            /// Returns true if tags are set in the inbox message.
+            /// </summary>
             public bool EnableTags { get; set; }
+
+            /// <summary>
+            /// The inbox content items.
+            /// For Simple Message and Icon Message templates the size of this List is by default 1.
+            /// For Carousel templates, the size of the List is the number of slides in the Carousel.
+            /// </summary>
             public List<Content> Content { get; set; }
+
+            /// <summary>
+            /// The inbox style Background Color.
+            /// </summary>
             public string BackgroundColor { get; set; }
+
+            /// <summary>
+            /// The inbox message orientation.
+            /// Returns "l" for landscape.
+            /// Returns "p" for portrait.
+            /// </summary>
             public string Orientation { get; set; }
 
             /// <summary>
@@ -65,13 +93,36 @@ namespace CleverTapSDK
             public List<CustomKV> CustomKeyValuePairs { get; set; }
         }
 
+        /// <summary>
+        /// The inbox message Content data.
+        /// </summary>
         public class Content
         {
             public long Key { get; set; }
+
+            /// <summary>
+            /// The inbox Message text and color.
+            /// </summary>
             public TextDetails Message { get; set; }
+
+            /// <summary>
+            /// The inbox Title text and color.
+            /// </summary>
             public TextDetails Title { get; set; }
+
+            /// <summary>
+            /// The inbox message Call to Action and Links.
+            /// </summary>
             public ActionDetails Action { get; set; }
+
+            /// <summary>
+            /// The inbox message main Image media data.
+            /// </summary>
             public Media Media { get; set; }
+
+            /// <summary>
+            /// The Inbox message Icon media data.
+            /// </summary>
             public Media Icon { get; set; }
         }
 
@@ -99,27 +150,63 @@ namespace CleverTapSDK
         /// </summary>
         public class Link
         {
+            /// <summary>
+            /// The Link type. Values could be "copy", "url", "kv".
+            /// </summary>
             public string Type { get; set; }
+
+            /// <summary>
+            /// The Link text.
+            /// </summary>
             public string Text { get; set; }
+
+            /// <summary>
+            /// The Link Text color.
+            /// </summary>
             public string Color { get; set; }
+
+            /// <summary>
+            /// The Link Fill color.
+            /// </summary>
             public string BackgroundColor { get; set; }
+
+            /// <summary>
+            /// The Copy to clipboard text value for "copy" type links.
+            /// </summary>
             public TextValue CopyText { get; set; }
+
+            /// <summary>
+            /// The Link URL for "Open URL" links.
+            /// </summary>
             public Url Url { get; set; }
+
+            /// <summary>
+            /// The Link Custom Key-Value pairs.
+            /// </summary>
             public Dictionary<string, string> KeyValuePairs { get; set; }
         }
 
+        /// <summary>
+        /// Text fields Text and Color.
+        /// </summary>
         public class TextDetails
         {
             public string Text { get; set; }
             public string Color { get; set; }
         }
 
+        /// <summary>
+        /// The URLs for iOS and Android.
+        /// </summary>
         public class Url
         {
             public TextValue Android { get; set; }
             public TextValue IOS { get; set; }
         }
 
+        /// <summary>
+        /// The Media data including URL and content type of the asset.
+        /// </summary>
         public class Media
         {
             public string Url { get; set; }
@@ -127,12 +214,18 @@ namespace CleverTapSDK
             public string ContentType { get; set; }
         }
 
+        /// <summary>
+        /// Custom Key-Value Pair.
+        /// </summary>
         public class CustomKV
         {
             public string Key { get; set; }
             public TextValue Value { get; set; }
         }
 
+        /// <summary>
+        /// The objects "text" value representation. 
+        /// </summary>
         public class TextValue
         {
             public string Text { get; set; }
