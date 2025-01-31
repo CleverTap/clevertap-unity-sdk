d6281205c1dcfbbcdcd186b2b5b0d46c70fc7e23
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-01-31 12:23:36 +0200

    iOS fixes and improvements (#139)
    * Fix compile bug for deprecated attribute ISO C99
    
    * Synchronize buffer
    
    * Return if nil callback info
    
    * Return correct type

--- CleverTap/Plugins/iOS/CleverTapMessageBuffer.m
+++ CleverTap/Plugins/iOS/CleverTapMessageBuffer.m
@@ -1,32 +1,43 @@
 #import "CleverTapMessageBuffer.h"
 
+@interface CleverTapMessageBuffer ()
+@property (nonatomic, strong, readonly) NSObject *lock;
+@end
+
 @implementation CleverTapMessageBuffer
 
 - (instancetype)initWithEnabled:(BOOL)isEnabled {
     if (self = [super init]) {
-        self.isEnabled = isEnabled;
-        self.items = [NSMutableArray array];
+        _isEnabled = isEnabled;
+        _items = [NSMutableArray array];
+        _lock = [[NSObject alloc] init];
     }
     return self;
 }
 
 - (void)addItem:(NSString *)item {
-    if (item) {
-        [self.items addObject:item];
+    @synchronized(self.lock) {
+        if (item) {
+            [self.items addObject:item];
+        }
     }
 }
 
 - (nullable NSString *)popItem {
-    if (self.items.count > 0) {
-        NSString *last = [self.items lastObject];
-        [self.items removeLastObject];
-        return last;
+    @synchronized(self.lock) {
+        if (self.items.count > 0) {
+            NSString *last = [self.items lastObject];
+            [self.items removeLastObject];
+            return last;
+        }
+        return nil;
     }
-    return nil;
 }
 
 - (NSUInteger)count {
-    return self.items.count;
+    @synchronized(self.lock) {
+        return self.items.count;
+    }
 }
 
 @end
