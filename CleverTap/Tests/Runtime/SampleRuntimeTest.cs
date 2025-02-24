86917630d40c24b5abd3667650d729416fad5463
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-02-24 11:11:52 +0200

    Merge pull request #153 from CleverTap/setup_tests
    Setup tests

---
+++ CleverTap/Tests/Runtime/SampleRuntimeTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class SampleRuntimeTest
+{
+    [Test]
+    public void Test_Example()
+    {
+        int a = 2;
+        int b = 3;
+        Assert.AreEqual(5, a + b);
+    }
+}
