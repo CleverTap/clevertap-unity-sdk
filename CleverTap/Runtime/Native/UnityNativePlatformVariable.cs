ac1497df03e895fc97a5f715d49c272c403fb6fe
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-02-25 11:35:54 +0200

    Merge pull request #152 from CleverTap/unity_native_variables_define
    Unity native variables define

--- CleverTap/Runtime/Native/UnityNativePlatformVariable.cs
+++ CleverTap/Runtime/Native/UnityNativePlatformVariable.cs
@@ -2,27 +2,41 @@
 using CleverTapSDK.Common;
 using CleverTapSDK.Utilities;
 
-namespace CleverTapSDK.Native {
-    internal class UnityNativePlatformVariable : CleverTapPlatformVariable {
-        internal override void SyncVariables() {
+namespace CleverTapSDK.Native
+{
+    internal class UnityNativePlatformVariable : CleverTapPlatformVariable
+    {
+        internal override void SyncVariables()
+        {
             CleverTapLogger.LogError("CleverTap Error: SyncVariables is not supported for this platform.");
         }
 
-        internal override void SyncVariables(bool isProduction) {
+        internal override void SyncVariables(bool isProduction)
+        {
             CleverTapLogger.LogError("CleverTap Error: SyncVariables is not supported for this platform.");
         }
 
-        internal override void FetchVariables(int callbackId) {
+        internal override void FetchVariables(int callbackId)
+        {
             CleverTapLogger.LogError("CleverTap Error: FetchVariables is not supported for this platform.");
         }
 
-        protected override Var<T> DefineVariable<T>(string name, string kind, T defaultValue) {
-            CleverTapLogger.LogError("CleverTap Error: Define is not supported for this platform.");
-            return null;
+        protected override Var<T> DefineVariable<T>(string name, string kind, T defaultValue)
+        {
+            UnityNativeVar<T> result = new UnityNativeVar<T>(name, kind, defaultValue);
+            varCache.Add(name, result);
+            return result;
         }
 
-        protected override Var<T> GetOrDefineVariable<T>(string name, T defaultValue) {
-            CleverTapLogger.LogError("CleverTap Error: Define is not supported for this platform.");
+        protected override Var<T> GetOrDefineVariable<T>(string name, T defaultValue)
+        {
+            var variable = base.GetOrDefineVariable<T>(name, defaultValue);
+            return variable;
+        }
+
+        protected override Var<string> GetOrDefineFileVariable(string name)
+        {
+            CleverTapLogger.LogError("CleverTap Error: File Variables are not supported for this platform.");
             return null;
         }
     }
