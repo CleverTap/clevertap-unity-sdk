06eecb2019cc98652552ea291f5c9dde772ce3fa
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-03-31 17:41:34 +0300

    Unity WebGL compatibility 6000 (#173)
    * Use UTF8ToString instead of Pointer_stringify. Use allocateString instead of allocate
    
    * Fix use of deprecated FindObjectOfType
    
    * Load settings correctly for WebGL

--- CTExample/Assets/Scripts/Utilities/CleverTapSettingsRuntime.cs
+++ CTExample/Assets/Scripts/Utilities/CleverTapSettingsRuntime.cs
@@ -1,5 +1,9 @@
 ﻿using System.IO;
 using UnityEngine;
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Threading.Tasks;
+using UnityEngine.Networking;
+#endif
 
 namespace CTExample
 {
@@ -7,13 +11,22 @@ namespace CTExample
     {
         // Path must be the same as CleverTapSDK CleverTapSettings JSON path
         private static readonly string jsonPath = Path.Combine(Application.streamingAssetsPath, "CleverTapSettings.json");
+
+#if !(UNITY_WEBGL && !UNITY_EDITOR)
         private static CleverTapSettingsRuntime _instance;
         private static bool loaded;
+#endif
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        private static Task<CleverTapSettingsRuntime> _instanceTask;
+        private static readonly object _lock = new object();
+#endif
 
         public string CleverTapAccountId;
         public string CleverTapAccountToken;
         public string CleverTapAccountRegion;
 
+#if !(UNITY_WEBGL && !UNITY_EDITOR)
         public static CleverTapSettingsRuntime Instance
         {
             get
@@ -26,6 +39,24 @@ namespace CTExample
                 return _instance;
             }
         }
+#endif
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        public static Task<CleverTapSettingsRuntime> Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instanceTask == null || _instanceTask.IsFaulted)
+                    {
+                        _instanceTask = LoadSettingsWebGL();
+                    }
+                }
+                return _instanceTask;
+            }
+        }
+#endif
 
         public bool IsValid()
         {
@@ -33,6 +64,7 @@ namespace CTExample
                    && !string.IsNullOrEmpty(CleverTapAccountToken);
         }
 
+#if !(UNITY_WEBGL && !UNITY_EDITOR)
         private static CleverTapSettingsRuntime LoadSettings()
         {
             if (File.Exists(jsonPath))
@@ -40,9 +72,34 @@ namespace CTExample
                 string json = File.ReadAllText(jsonPath);
                 return JsonUtility.FromJson<CleverTapSettingsRuntime>(json);
             }
-            Debug.LogWarning("CleverTapSettings JSON not found. \n" +
+            Logger.LogWarning("CleverTapSettings JSON not found. \n" +
                 "If you want to use CleverTapSettings runtime, check the CleverTapSettings \"Save to streaming assets\" option.");
             return null;
         }
+#endif
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        private static async Task<CleverTapSettingsRuntime> LoadSettingsWebGL()
+        {
+            UnityWebRequest request = UnityWebRequest.Get(jsonPath);
+            UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+            while (!operation.isDone)
+            {
+                await Task.Yield();
+            }
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var json = request.downloadHandler.text;
+                return JsonUtility.FromJson<CleverTapSettingsRuntime>(json);
+            }
+            else
+            {
+                Logger.LogWarning("Cannot load CleverTapSettings JSON at " + jsonPath);
+                return null;
+            }
+        }
+#endif
     }
 }
