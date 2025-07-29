ba539fafd816d6449d61efd5a8f94ecab991988d
Suriya K <58582081+SuriyaKannimuthu@users.noreply.github.com>
2025-07-29 14:15:51 +0530

    Removed unused - constant CLEVERTAP_SDK_BUILD_VERSION

--- CleverTap/Runtime/Constants/CleverTapVersion.cs
+++ CleverTap/Runtime/Constants/CleverTapVersion.cs
@@ -2,6 +2,5 @@ namespace CleverTapSDK.Constants {
     public static class CleverTapVersion {
         public const string CLEVERTAP_SDK_VERSION = "5.3.0";
         public const int CLEVERTAP_SDK_REVISION = 50300;
-        public const string CLEVERTAP_SDK_BUILD_VERSION = "5030000";
     }
 }
