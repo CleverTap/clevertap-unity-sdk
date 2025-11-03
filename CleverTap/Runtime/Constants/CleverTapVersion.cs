3783e7673630ba1bc413c57b25c1fd8976343878
Suriya K <58582081+SuriyaKannimuthu@users.noreply.github.com>
2025-11-03 11:58:11 +0530

    Release - 5.3.7
    - Added - "Preserve" attribute to address IL2CPP stripping of non-referenced property setters in UnityNativeEventDBEntry.
    - Updated to [CleverTap Android SDK v7.6.0](https://github.com/CleverTap/clevertap-android-sdk/tree/corev7.6.0_ptv2.2.0)
    - Updated to [CleverTap iOS SDK v7.3.4](https://github.com/CleverTap/clevertap-ios-sdk/releases/tag/7.3.4)

--- CleverTap/Runtime/Constants/CleverTapVersion.cs
+++ CleverTap/Runtime/Constants/CleverTapVersion.cs
@@ -1,6 +1,6 @@
 namespace CleverTapSDK.Constants {
     public static class CleverTapVersion {
-        public const string CLEVERTAP_SDK_VERSION = "5.3.6";
-        public const int CLEVERTAP_SDK_REVISION = 50306;
+        public const string CLEVERTAP_SDK_VERSION = "5.3.7";
+        public const int CLEVERTAP_SDK_REVISION = 50307;
     }
 }
